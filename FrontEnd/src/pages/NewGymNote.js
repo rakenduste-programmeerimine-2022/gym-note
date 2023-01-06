2e10c8b4c84e707777f73d605a79177901ff954c
KarlTheFish <KarlTheFish@users.noreply.github.com>
2023-01-06 03:02:42 +0200

    Making a commit here incase I fuck up this thing I'm about to try

--- FrontEnd/src/pages/NewGymNote.js
+++ FrontEnd/src/pages/NewGymNote.js
@@ -1,8 +1,43 @@
 import * as React from 'react';
 import CssBaseline from "@mui/material/CssBaseline";
-import {Container, Grid, TextField} from "@mui/material";
+import {Button, Container, Table, TableBody, TableCell, TableHead, TableRow, TextField} from "@mui/material";
 import Box from "@mui/material/Box";
-import Grid2 from "@mui/material/Unstable_Grid2";
+
+
+function ExerciseTable(){
+    return(
+        <Box paddingX={10} fontFamily={"Inter"}>
+            <Table>
+                <TableHead>
+                    <TableRow sx={{ backgroundColor: "gray" }}>
+                        <TableCell>Exercise</TableCell>
+                        <TableCell>Reps</TableCell>
+                        <TableCell>Weight</TableCell>
+                        <TableCell>Failure</TableCell>
+                        <TableCell>Notes</TableCell>
+                    </TableRow>
+                </TableHead>
+                <CurrentRow></CurrentRow>
+            </Table>
+        </Box>
+    )
+}
+
+function CurrentRow(){
+    return(
+    <TableBody>
+        <TableRow>
+            <TableCell>
+                <Button variant="contained"   onClick={() => AddRow() }>Add exercise...</Button>
+            </TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell> {/* These bunch of tablecells are just for cosmetic effect, plz no touchy*/}
+        </TableRow>
+    </TableBody>
+    )
+}
 
 export default function NewGymNote(){
     return (
@@ -13,22 +48,11 @@ export default function NewGymNote(){
                     <Box textAlign={"left"} lineHeight={3} paddingLeft={3} color={"black"} fontSize={30}>
                         TODAY'S GYMNOTE:</Box>
 
-                    <Box paddingX={10} fontFamily={"Inter"}>
-                        <TextField sx={{ backgroundColor: 'white', fontFamily: 'inter'}} //TODO: fix the font, set to Inter
+                    <Box paddingX={10} fontFamily={"Inter"} paddingBottom={5}>
+                        <TextField sx={{ backgroundColor: 'white'}} //TODO: fix the font, set to Inter
                                    id="NoteDescription" label="Description" variant="outlined" fullWidth multiline> Why can I put random text here lol </TextField>
-                        <Grid2 container spacing={6}>
-                            <Grid xs={4}>
-                                <p>Test</p>
-                                <p>Test 2</p>
-                                <p>Test 3</p>
-                            </Grid>
-                            <Grid xs={4}>
-                                <p> Row 1</p>
-                                <p> Row 2</p>
-                                <p> Row 3</p>
-                            </Grid>
-                        </Grid2>
                     </Box>
+                    <ExerciseTable/>
                 </Box>
             </Container>
         </React.Fragment>
