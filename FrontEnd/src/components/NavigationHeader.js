2028ea053b3c305a1f672095c842fcf0aa37aeb8
Egon Ubina <90237357+EgoUbin@users.noreply.github.com>
2023-01-20 02:16:12 +0200

    Default page + explore route
    SOme routing fixes and QOL

--- FrontEnd/src/components/NavigationHeader.js
+++ FrontEnd/src/components/NavigationHeader.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import ReactDOM from "react-dom";
 import { Tabs, Tab, AppBar} from "@mui/material";
 import { Route, BrowserRouter, Switch, NavLink } from "react-router-dom";
 import { styled } from "@mui/material/styles";
 import NewGymNote from "../pages/NewGymNote";
 import ProfilePage from "../pages/Profile";
+import Explore from "../pages/Explore";
 
 
 const StyledTab = styled(Tab)({
@@ -17,20 +17,23 @@ const StyledTab = styled(Tab)({
     }
 })
 export default function NavigationHeader() {
-    const routes = ["/Profile","/NewGymNote"];
+    const routes = ["/Profile","/NewGymNote","/Explore"];
     return (
       <BrowserRouter>
       <AppBar>
         <Tabs>
-          <StyledTab label={<NavLink to="/Profile">Profile</NavLink>} />
+          <StyledTab label={<NavLink to={routes[0]}>Profile</NavLink>} />
           <StyledTab label={<NavLink to={routes[1]}>New Gym Note</NavLink>} />
+          <StyledTab label={<NavLink to={routes[2]}>Explore</NavLink>} />
+
         </Tabs>
         <Switch>
           <Route exact path={routes[0]} component={ProfilePage} />
           <Route exact path={routes[1]} component={NewGymNote} />
+          <Route exact path={routes[2]} component={Explore} />
+          <Route exact path="/" component={ProfilePage} />
         </Switch>
       </AppBar>
     </BrowserRouter>
     );
   }
-
